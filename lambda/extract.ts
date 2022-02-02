5f560bdfb709b62e030e3bfd21b6613d2aa9aad6
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-02 14:28:20 +0500

    extract lambda

--- lambda/extract.ts
+++ lambda/extract.ts
@@ -50,4 +50,95 @@ export async function handler(event: any) {
       },
     },
   };
+
+  /**
+   * An event can contain multiple records to be process, i.e. the user could have uploaded 2 files to S3.
+   */
+  for (let record of records) {
+    let payload = JSON.parse(records[record].body);
+    console.log(
+      `Payload for processing s3 events: ${JSON.stringify(payload, null, 2)}`
+    );
+
+    let s3EventRecords = payload.Records;
+    console.log(`S3 Event Records: ${JSON.stringify(s3EventRecords, null, 2)}`);
+
+    for (let i in s3EventRecords) {
+      let s3Event = s3EventRecords[i];
+      console.log(`S3 Event: ${JSON.stringify(s3Event, null, 2)}`);
+
+      // extract variables from event
+      const bucketName = s3Event?.s3?.bucket?.name;
+      const objectKey = s3Event?.s3?.object?.key;
+      const bucketArn = s3Event?.s3?.object?.arn;
+
+      console.log(`Bucket Name: ${bucketName}`);
+      console.log(`Object Key: ${objectKey}`);
+      console.log(`Bucket ARN: ${bucketArn}`);
+
+      if (
+        typeof objectKey != "undefined" &&
+        typeof bucketName != "undefined" &&
+        typeof bucketArn != "undefined"
+      ) {
+        params.overrides = {
+          containerOverrides: [
+            {
+              name: containerName,
+              environment: [
+                {
+                  name: "S3_BUCKET_NAME",
+                  value: bucketName,
+                },
+                {
+                  name: "S3_OBJECT_KEY",
+                  value: objectKey,
+                },
+              ],
+            },
+          ],
+        };
+
+        let ecsResponse = await ecs
+          .runTask(params)
+          .promise()
+          .catch((err) => {
+            throw new Error(err);
+          });
+        console.log(`ECS Response: ${JSON.stringify(ecsResponse, null, 2)}`);
+
+        // Create an event to be sent to the Event Bridge
+        const eventBridgeParams: any = {
+          Entries: [
+            {
+              EventBusName: "default",
+              Source: "S3-Event-Bridge",
+              DetailType: "S3-Event-Bridge",
+              Time: new Date(),
+              Detail: JSON.stringify({
+                BucketName: bucketName,
+                status: "success",
+                data: ecsResponse,
+              }),
+            },
+          ],
+        };
+
+        // Send the event to the Event Bridge
+        const result = await eventBridge
+          .putEvents(eventBridgeParams)
+          .promise()
+          .catch((err) => {
+            throw new Error(err);
+          });
+        console.log(
+          `Event Bridge Response: ${JSON.stringify(result, null, 2)}`
+        );
+      } else {
+        console.log(
+          `Skipping record ${JSON.stringify(s3EventRecords[i], null, 2)}`
+        );
+      }
+    }
+  }
 }

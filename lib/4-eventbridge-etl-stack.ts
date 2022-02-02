f6efc6d844efb73f52366467bedf90352bec104d
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-02 11:13:59 +0500

    LAMBDA_THROTTLE_SIZE

--- lib/4-eventbridge-etl-stack.ts
+++ lib/4-eventbridge-etl-stack.ts
@@ -1,16 +1,26 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3Notifications from "aws-cdk-lib/aws-s3-notifications";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as lambdaEventSources from "aws-cdk-lib/aws-lambda-event-sources";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as logs from "aws-cdk-lib/aws-logs";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as events from "aws-cdk-lib/aws-events";
+import * as eventsTargets from "aws-cdk-lib/aws-events-targets";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
-export class 4EventbridgeEtlStack extends Stack {
+export class EventBridgeEtlStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, '4EventbridgeEtlQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // ========================================================================
+    // Lambda Throttle size, if left unchecked, this pattern could "fan out" on the transform and load lambdas to the point that it consumes all the available resources on the account. this is wht we are limiting concurrency to 2 on all 3 lambdas.
+    // ========================================================================
+    const LAMBDA_THROTTLE_SIZE = 2;
   }
 }

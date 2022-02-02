fcac263634dcae7d7b0570bf6c6cbff66bf9a45c
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-02 10:42:15 +0500

    Initial commit

---
+++ test/4-eventbridge-etl.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as 4EventbridgeEtl from '../lib/4-eventbridge-etl-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/4-eventbridge-etl-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new 4EventbridgeEtl.4EventbridgeEtlStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
